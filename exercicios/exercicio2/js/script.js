b8571704d53b280f703dc7dc42ba987761efa64e
Igor da Silva <112149779+igorfirminosilva@users.noreply.github.com>
2022-11-20 23:22:56 -0300

    Update script.js

--- exercicios/exercicio2/js/script.js
+++ exercicios/exercicio2/js/script.js
@@ -48,8 +48,46 @@ function verificar(){
             }
         }
 
-        res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
         res.appendChild(img)
+    }
+}
+
+
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('ano')
+    var res = document.getElementById('res')
+
+    if (fano.value.length == 0 || fano.value > ano){
+        alert('Preenche isso direito porra!!!!')
+    } else {
+        var fsex = document.getElementsByName('radesex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+
+        if (fsex[0].checked){
+            gênero = 'Homem'
+    
+        
+        if (idade >= 0 && idade < 10){
+            //Criança
+            img.setAttribute('src', 'imagens/menino.jpg')
+        }
+        
+    } else if (fsex[1].checked){
+        gênero = 'Mulher'
+
+        if (idade >= 0 && idade < 10){
+            img.setAttribute('src', 'imagens/menina.jpg')
+        }
+    }
+
+    res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+    res.appendChild(img)
     }
 }
