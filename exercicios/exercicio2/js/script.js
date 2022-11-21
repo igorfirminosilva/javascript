1631df61a415450779a5ac1a201a30096864f5f1
Igor da Silva <112149779+igorfirminosilva@users.noreply.github.com>
2022-11-20 22:35:02 -0300

    exe2

---
+++ exercicios/exercicio2/js/script.js
@@ -0,0 +1,55 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('ano')
+    var res = document.getElementById('res')
+
+    if(fano.value.length == 0 || fano.value > ano){
+        alert('[ERRO] - Preencha corretamente os campos')
+    } else{
+        var fsex = document.getElementsByName('radesex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+
+            if(idade >= 0 && idade < 10){
+                //Criança
+                img.setAttribute('src', 'menino.jpg')
+            } else if (idade < 18){
+                //Jovem
+                img.setAttribute('src', 'menino.jpg')
+            } else if (idade < 50){
+                //Adulto
+                img.setAttribute('src', 'menino.jpg')
+            } else{
+                //Idoso
+                img.setAttribute('src', 'menino.jpg')
+            }
+
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+
+            if(idade >= 0 && idade < 10){
+                //Criança
+                img.setAttribute('src', 'menino.jpg')
+            } else if (idade < 18){
+                //Jovem
+                img.setAttribute('src', 'menino.jpg')
+            } else if (idade < 50){
+                //Adulto
+                img.setAttribute('src', 'menino.jpg')
+            } else{
+                //Idoso
+                img.setAttribute('src', 'menino.jpg')
+            }
+        }
+
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+        res.appendChild(img)
+    }
+}
