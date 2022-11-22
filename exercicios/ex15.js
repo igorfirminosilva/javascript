4972a1d930b778591864f03a6d2b60b0d9f23b88
Igor da Silva <112149779+igorfirminosilva@users.noreply.github.com>
2022-11-21 23:17:24 -0300

    ex14

---
+++ exercicios/ex15.js
@@ -0,0 +1,17 @@
+/*console.log('Vai começar....')
+
+for (var c = 1 ; c <= 4 ; c++) {
+    console.log(`${c}`)
+}
+
+console.log('Fim')
+
+*/
+
+var c = 1 
+
+for (var c = 1 ; c <= 4 ; c++){
+    console.log(`Passo ${c}`)
+}
+
+
