274c1f94333459abb87b18c9e41766dbc6d31e29
Igor da Silva <112149779+igorfirminosilva@users.noreply.github.com>
2022-11-21 22:15:18 -0300

    ex 14

---
+++ exercicios/ex14.js
@@ -0,0 +1,15 @@
+/*var c = 1
+
+do{
+    console.log(`Aqui tem ${c}`)
+    c++
+}while(c <= 10)
+*/
+
+var nome = 'Igor'
+
+while(nome = 'Igor'){
+    console.log(`O nome ${nome} vai ser digitado 6 vezes`)
+    nome++
+}
+
